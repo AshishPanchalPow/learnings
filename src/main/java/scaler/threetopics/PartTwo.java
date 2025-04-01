33eb7af91bf1c05ff38c7ff77c08f75824cd17c9
Ashish PANCHAL <Ashish.PANCHAL@amadeus.com>
2025-04-01 09:27:37 +0530

    Queues

--- src/main/java/scaler/threetopics/PartTwo.java
+++ src/main/java/scaler/threetopics/PartTwo.java
@@ -1,15 +1,68 @@
 package scaler.threetopics;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.Stack;
+import java.util.*;
 
 public class PartTwo {
     public static void main(String[] args) {
         int[] nums={3,15,6,12,4,2,10,8,5};
         int K=4;
-        maxElementInWindow(nums,K);
+        String A="xxikrwmjvsvckfrqxnibkcasompsuyuogauacjrr";
+        System.out.println(stringOnBoard(A));
+    }
+    public static String stringOnBoard(String A) {
+        StringBuilder stringBuilder=new StringBuilder();
+        Queue<Character> queue=new LinkedList<>();
+        Map<Character,Integer> characterCount=new HashMap<>();
+        for(int i=97; i<97+26;i++){
+            characterCount.put((char)i,0);
+        }
+        queue.add(A.charAt(0));
+        characterCount.put(A.charAt(0),1);
+        stringBuilder.append(A.charAt(0));
+        for(int i=1;i<A.length();i++){
+
+            int freq=characterCount.get(A.charAt(i));
+            freq++;
+            characterCount.put(A.charAt(i),freq);
+            if(queue.isEmpty()){
+                stringBuilder.append(A.charAt(i));
+                queue.add(A.charAt(i));
+            }
+            else{
+                if(!queue.isEmpty() && !queue.peek().equals(A.charAt(i))){
+                    stringBuilder.append(queue.peek());
+                    queue.add(A.charAt(i));
+                }
+                else{
+                    queue.poll();
+                    Character ch=queue.peek();
+                    while(!queue.isEmpty() && characterCount.get(queue.peek())!=1){
+                        queue.poll();
+                    }
+                    if(!queue.isEmpty())stringBuilder.append(queue.peek());
+                    else stringBuilder.append("#");
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+    public static String perfectNumber(int A) {
+        Queue<String> primaryQueue=new LinkedList<>();
+        primaryQueue.add("1");
+        primaryQueue.add("2");
+        int counter=0;
+        String result="";
+        while(counter !=A){
+            String out=primaryQueue.poll();
+            result=out+new StringBuilder(out).reverse().toString();
+            if(counter==A){
+                break;
+            }
+            primaryQueue.add(out+"1");
+            primaryQueue.add(out+"2");
+            counter++;
+        }
+        return result;
     }
     public static void maxElementInWindow(int[] nums, int K){
         int N=nums.length;
