ce9e30fabf9c42822855c68ce6d6e8ee5051450c
Ashish PANCHAL <Ashish.PANCHAL@amadeus.com>
2025-04-06 18:10:41 +0530

    TreesFour

--- src/main/java/scaler/threetopics/PartFour.java
+++ src/main/java/scaler/threetopics/PartFour.java
@@ -1,5 +1,10 @@
 package scaler.threetopics;
 
+import org.amadeus.oops.polymorphism.A;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PartFour {
     static int count=0; static int ans=0;
     public static void main(String[] args) {
@@ -13,8 +18,68 @@ public class PartFour {
         root.right.right.left=new TreeNode(28);
         root.right.right.right=new TreeNode(40);
         //System.out.println(isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE));
-        kThSmallestInBST(root,7);
-        System.out.println(ans);
+        //kThSmallestInBST(root,7);
+        //System.out.println(ans);
+        //System.out.println(LCAinBST(root,12,40).val);
+        System.out.println(solve(root,20,17));
+
+    }
+    public static int solve(TreeNode A, int B, int C) {
+        TreeNode lca=LCAinBST(A,B,C);
+        //Calc Dist to B.
+        TreeNode currB=lca;
+        int distB=0;
+        while(currB !=null && currB.val !=B){
+            if(currB.val >B){
+                currB=currB.left;
+            }
+            else {
+                currB=currB.right;
+            }
+            distB++;
+        }
+        TreeNode currA=lca;
+        int distA=0;
+        while(currA !=null && currA.val !=C){
+            if(currA.val >C){
+                currA=currA.left;
+            }
+            else {
+                currA=currA.right;
+            }
+            distA++;
+        }
+        return distB+distA;
+
+    }
+    private static TreeNode LCAinBST(TreeNode root, int A, int B){
+        TreeNode curr=root;
+        while (curr !=null){
+            if(A<curr.val && B<curr.val){
+                return LCAinBST(root.left,A,B);
+            }
+            else if(A>curr.val && B>curr.val){
+                return LCAinBST(root.right,A,B);
+            }
+            else {
+                return root;
+            }
+        }
+        return root;
+    }
+    private static boolean path(TreeNode root,int K, List<Integer> paths){
+        if(root==null){
+            return false;
+        }
+        paths.add(root.val);
+        if (root.val == K) {
+            return true;
+        }
+        if (path(root.left, K, paths) || path(root.right, K, paths)) {
+            return true;
+        }
+        paths.remove(paths.size() - 1);
+        return false;
     }
     private static void kThSmallestInBST(TreeNode root, int K){
         if(root==null){
